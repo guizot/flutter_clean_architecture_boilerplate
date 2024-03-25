7140b46da6aba668b2bf39cbe425813ca7563f48
Guizot <guizot@outlook.com>
2024-03-25 23:32:45 +0700

    fix error

--- lib/presentation/core/services/theme_service.dart
+++ lib/presentation/core/services/theme_service.dart
@@ -90,9 +90,9 @@ class ThemeService extends ChangeNotifier {
       chipTheme: ChipThemeData(
         selectedColor: ColorUtils().getMaterialColor(colorTheme).shade500
       ),
-      textTheme: const TextTheme(
-        bodyLarge: TextStyle(fontFamily: '')
-      )
+      // textTheme: const TextTheme(
+      //   bodyLarge: TextStyle(fontFamily: '')
+      // )
     );
   }
 
